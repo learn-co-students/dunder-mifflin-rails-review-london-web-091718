d6e7658c7cdb1fcb260160a675ed61b6cd787ffa
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-10-18 14:05:08 +0100

    done!

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
 end
