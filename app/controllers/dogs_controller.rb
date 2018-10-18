1b49f852a57bd5391b5ca8cf5c6150608294ba77
BarneyLoosemore <biscuitfull@gmail.com>
2018-10-18 13:25:47 +0100

    Done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,37 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to dog_path(@dog)
+  end
+
+  def update
+    @dog.update(dog_params)
+    redirect_to dog_path(@dog)
+  end
+
+  def destroy
+    @dog.destroy
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
