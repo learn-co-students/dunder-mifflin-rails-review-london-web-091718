eaef43d19b98dfc50f8491caa76d2977b6b5c8c0
Ed Putans <edputans@gmail.com>
2018-10-18 14:10:27 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,43 @@
 class DogsController < ApplicationController
+
+def show
+  @dog=Dog.find(params[:id])
+end
+
+def index
+  @dogs = Dog.all
+end
+
+def new
+  @dog = Dog.new
+end
+
+def create
+  @dog = Dog.create(dog_params)
+  redirect_to dog_path
+end
+
+def edit
+  @dog = Dog.find(params[:id])
+end
+
+def update
+  @dog=Dog.find(params[:id])
+  @dog.update(dog_params)
+  redirect_to dog_path
+end
+
+def destroy
+  @dog = Dog.find(params[:id])
+  @dog.destroy
+  redirect_to dogs_path
+end
+
+private
+
+def dog_params
+  params.require(:dog).permit(:name, :breed, :age, employee_ids: [])
+end
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee=Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+    @dog = @employee.dog
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+    @dog = @employee.dog
+  end
+
+  def update
+      @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path(@employee)
+  end
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
+
 end
