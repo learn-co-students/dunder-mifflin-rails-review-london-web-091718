e75f3ccc90c16bd6be88051f7302e2c0c26a3ad6
Patrick <ctrlaltpat@gmail.com>
2018-10-18 17:03:32 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+  # before_action :find_dog only: [:show]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  private
+
+  def class_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
 end
