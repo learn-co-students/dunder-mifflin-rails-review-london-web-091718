db676bb6c49ecc1105dc0e1a42105c48baedc139
deedeeh <dinahafez.8@gmail.com>
2018-10-18 14:04:41 +0100

    Done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,52 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :set_dogs, only: [:new, :edit]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path
+    end
+  end
+
+  def update
+    @employee.update(employee_params)
+    if @employee.save
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
+  def set_dogs
+    @dogs = Dog.all
+  end
+
 end
